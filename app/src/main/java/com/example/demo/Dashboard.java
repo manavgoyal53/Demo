c945ab3003e8de6f45609c9102bc52b86c394287
Manav Goyal <manavgoyal53@gmail.com>
2020-05-17 18:42:37 +0530

    Some Final changes

--- app/src/main/java/com/example/demo/Dashboard.java
+++ app/src/main/java/com/example/demo/Dashboard.java
@@ -2,13 +2,36 @@ package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Dashboard extends AppCompatActivity {
+    Button btnSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        findViews();
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Dashboard.this,MainActivity.class));
+                finish();
+            }
+        });
     }
+
+    private void findViews() {
+        btnSignOut=findViewById(R.id.btn_sign_out);
+    }
+
 }
